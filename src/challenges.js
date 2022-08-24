14c40327682ca17584982337b7472b997be30bd5
Larissa <larisssa_rga@hotmail.com>
2022-08-24 16:00:19 -0300

    Adiciona segundo e terceiro requisito obrigatorio

--- src/challenges.js
+++ src/challenges.js
@@ -6,13 +6,16 @@ function compareTrue(valor1, valor2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let arrayDeStrings = [];
+  arrayDeStrings = string.split(' ');
+  return arrayDeStrings;
 }
 
 // Desafio 4
